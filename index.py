d87b5a14c38da6a93bb7bee974a1e08ee19233a1
oyamin <oleg_yamin@yahoo.com>
2016-06-04 17:53:32 -0700

    New Functions

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
 import requests
 import random
 from lxml import html
+import sys
+import os
 
 url = "http://www.bash.org/?"
 num = str(random.randrange(1, 2000))
@@ -10,20 +12,39 @@ headers = {'Accept-Encoding': 'identity'}
 resp = requests.get('http://www.bash.org/?' + num, headers=headers)
 tree = html.fromstring(resp.content)
 
-quote = tree.xpath('//p[@class="qt"]/text()')
-noquote = tree.xpath('//font[@class="bodytext"]/text()')
-print noquote
-for quotebody in quote:
-    item = quotebody.strip()
-    restart = True
-    while restart:  
-        if noquote is True:
-            print noquote
-            continue
-        else:
-            print item
-            restart = False
-            break
+def quote():
+    q_true = tree.xpath('//p[@class="qt"]/text()')
+    for q_out in q_true:
+        item = q_out.strip()
+        print item
+    return
+
+def noquote():
+    noq_true = tree.xpath('//font[@class="bodytext"]/text()')
+    for noq_out in noq_true:
+        print noq_out
+    return
+       
+restart = True     
+while restart:
+    restart = False
+    f_quote = quote()
+    f_noquote = noquote()
+    if f_noquote is None:
+        f_quote
+    elif f_noquote:
+        f_noquote
+        restart = True
+        
+       
+
+
+    
+
+
+
+            
+            
 
 
 
